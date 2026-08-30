e059d73a3fafc0f8aa6b2dff5eb370e4fcc598f4
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-30 13:33:37 +0100

    Add configuration for indices and Maroc files

---
+++ config.py
@@ -0,0 +1,12 @@
+INDICES = {
+    "CAC 40": "^FCHI",
+    "S&P500": "^GSPC",
+    "NASDAQ": "^IXIC",
+    "Dow Jones": "^DJI",
+    "DAX": "^GDAXI"
+}
+
+MAROC_FILES = {
+    "MASI": "data/MASI.xlsx",
+    "MASI20": "data/MASI20.xlsx"
+}
