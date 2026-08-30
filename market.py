d619141fe030207cf62af834a5a4fbc2b47f62d8
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-30 14:58:02 +0100

    Enhance metrics computation in market.py
    Refactor compute_metrics to include performance calculations for 1 month and 3 months, and adjust volatility calculation to use weekly data.

--- market.py
+++ market.py
@@ -21,43 +21,84 @@ def load_maroc_index(filepath):
 
     df["Date"] = pd.to_datetime(df["Date"])
 
-    df.set_index(
-        "Date",
-        inplace=True
-    )
+    df = df.sort_values("Date")
+
+    df.set_index("Date", inplace=True)
 
     return df
 
 
 def compute_metrics(df):
 
-    close = df["Close"]
-
-    if isinstance(close, pd.DataFrame):
-        close = close.iloc[:, 0]
-
-    close = close.dropna()
+    close = df["Close"].dropna()
 
     current_year = close.index[-1].year
 
-    ytd = close[
+    ytd_data = close[
         close.index.year == current_year
     ]
 
     perf_ytd = (
         close.iloc[-1] /
-        ytd.iloc[0] - 1
+        ytd_data.iloc[0]
+        - 1
     ) * 100
+
+    if len(close) >= 5:
+
+        perf_1m = (
+            close.iloc[-1] /
+            close.iloc[-5]
+            - 1
+        ) * 100
+
+    else:
+
+        perf_1m = 0
+
+    if len(close) >= 13:
+
+        perf_3m = (
+            close.iloc[-1] /
+            close.iloc[-13]
+            - 1
+        ) * 100
+
+    else:
+
+        perf_3m = 0
 
     returns = close.pct_change().dropna()
 
     volatility = (
         returns.std()
-        * np.sqrt(252)
+        * np.sqrt(52)
         * 100
     )
 
+    max_close = close.cummax()
+
+    drawdown = (
+        (close - max_close)
+        / max_close
+    )
+
+    max_drawdown = (
+        drawdown.min() * 100
+    )
+
     return {
-        "Performance YTD (%)": round(perf_ytd, 2),
-        "Volatilité (%)": round(volatility, 2)
-    }
+
+        "Performance 1 mois (%)":
+        round(perf_1m, 2),
+
+        "Performance 3 mois (%)":
+        round(perf_3m, 2),
+
+        "Performance YTD (%)":
+        round(perf_ytd, 2),
+
+        "Volatilité (%)":
+        round(volatility, 2),
+
+        "Drawdown 
