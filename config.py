2c36b88785fd43760e82ab0a07227740667fe5c1
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-30 13:54:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
-INDICES = {
-    "CAC 40": "^FCHI",
-    "S&P500": "^GSPC",
-    "NASDAQ": "^IXIC",
-    "Dow Jones": "^DJI",
-    "DAX": "^GDAXI"
+MAROC_INDICES = {
+    "MASI": "data/MASI.xlsx",
+    "MASI20": "data/MASI20.xlsx",
+    "Banques": "data/Banques.xlsx",
+    "Assurances": "data/Assurances.xlsx",
+    "Immobilier": "data/Immobilier.xlsx",
+    "Mines": "data/Mines.xlsx"
 }
 
-MAROC_FILES = {
-    "MASI": "data/MASI.xlsx",
-    "MASI20": "data/MASI20.xlsx"
+WORLD_INDICES = {
+    "CAC40": "^FCHI",
+    "SP500": "^GSPC",
+    "NASDAQ": "^IXIC"
 }
